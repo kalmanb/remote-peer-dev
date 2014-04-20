2d93bafda71dc92e55aef718d75ecad04dc4618e
kalmanb <kalman@movio.co>
2014-04-20 22:13:11 +1200

    Update Connection case classes

--- src/test/scala/com/kalmanb/peerdev/ConnectTest.scala
+++ src/test/scala/com/kalmanb/peerdev/ConnectTest.scala
@@ -9,83 +9,98 @@ class ConnectTest extends TestSpec {
   describe("connection test") {
 
     it("should support connnction") {
-      val request =
-        """{ "username": "user", 
-      "platform": "linux2", 
-      "secret": "xxx", 
-      "client": "Sublime Text 2", 
-      "room": "room1", 
-      "version": "0.11", 
-      "supported_encodings": ["utf8", "base64"], 
-      "room_owner": "user", 
-      "api_key": "xxx"}""".parseJson
+      val request = """{
+        "username": "user", 
+        "platform": "linux2", 
+        "secret": "xxx", 
+        "client": "unix", 
+        "room": "room1", 
+        "version": "0.11", 
+        "supported_encodings": ["utf8", "base64"], 
+        "room_owner": "user", 
+        "api_key": "xxx"}""".parseJson
 
-      println(request)
+      val response = """{
+        "anon_perms":["get_buf","ping","pong"],
+        "bufs":{
+          "1":{"path":"README.md","id":1,"md5":"abc","encoding":"utf8"}
+        },
+          "max_size":52428800,
+          "owner":"user",
+          "room_name":"main-room",
+          "secret":false,
+          "server_id":"main-server",
+          "temp_data":{},
+          "terms":{},
+          "tree":{"README.md":1},
+          "users":{
+            "178":{
+              "client":"client",
+              "user_id":178,
+              "is_anon":false,
+              "perms":["get_buf","ping","pong","kick","pull_repo","perms","request_perms","patch","set_buf","create_buf","delete_buf","rename_buf","set_temp_data","delete_temp_data","highlight","msg","datamsg","create_term","term_stdin","delete_term","update_term","term_stdout","saved"],
+              "platform":"linux2",
+              "username":"user",
+              "version":0.11 }
+          },
+          "perms":["get_buf","ping","pong","kick","pull_repo","perms","request_perms","patch","set_buf","create_buf","delete_buf","rename_buf","set_temp_data","delete_temp_data","highlight","msg","datamsg","create_term","term_stdin","delete_term","update_term","term_stdout","saved"],
+          "user_id":1,
+          "name":"main-room"}""".parseJson
 
-      val response = """{"anon_perms":["get_buf","ping","pong"],
-      "bufs":{
-        "1":{"path":"README.md","id":1,"md5":"abc","encoding":"utf8"}
-      },
-        "max_size":52428800,
-        "owner":"user",
-        "room_name":"test1",
-        "secret":false,
-        "server_id":"server1",
-        "temp_data":{},
-        "terms":{},
-        "tree":{"README.md":1},
-        "users":{
-          "178":{
-            "client":"Sublime Text 2",
-            "user_id":178,
-            "is_anon":false,
-            "perms":["get_buf","ping","pong","kick","pull_repo","perms","request_perms","patch","set_buf","create_buf","delete_buf","rename_buf","set_temp_data","delete_temp_data","highlight","msg","datamsg","create_term","term_stdin","delete_term","update_term","term_stdout","saved"],
-            "platform":"linux2",
-            "username":"user",
-            "version":0.11 }
-         },
-         "perms":["get_buf","ping","pong","kick","pull_repo","perms","request_perms","patch","set_buf","create_buf","delete_buf","rename_buf","set_temp_data","delete_temp_data","highlight","msg","datamsg","create_term","term_stdin","delete_term","update_term","term_stdout","saved"],
-         "user_id":178,
-         "name":"room_info"}""".parseJson
+      object Connection {
 
-      println(response)
+        case class Buffer(
+          id: Int,
+          path: String,
+          md5: String,
+          encoding: String = "utf8")
 
-      case class Buffer(
-        id: Int,
-        path: String,
-        md5: String,
-        encoding: String = "utf8")
+        case class Tree(
+          filename: String,
+          id: Int)
 
-      case class Tree(
-        filename: String,
-        id: Int)
+        val perms = List("get_buf", "ping", "pong", "kick", "pull_repo", "perms", "request_perms", "patch", "set_buf", "create_buf", "delete_buf", "rename_buf", "set_temp_data", "delete_temp_data", "highlight", "msg", "datamsg", "create_term", "term_stdin", "delete_term", "update_term", "term_stdout", "saved")
 
-      case class ConnectionResponse(
-        bufs: List[Buffer],
-        max_size: Int,
-        owner: String,
-        room_name: String,
-        secret: Boolean,
-        server_id: String,
-        temp_data: List[String] = List.empty,
-        terms: List[String] = List.empty,
-        tree: List[Tree])
+        val anon_perms = List("get_buf", "ping", "pong")
 
-      object MyJsonProtocol extends DefaultJsonProtocol {
-        implicit val bufferFormat = jsonFormat4(Buffer)
-        implicit val treeFormat = jsonFormat2(Tree)
-        implicit val connectionResponseFormat = jsonFormat9(ConnectionResponse)
+        case class User(
+          user_id: Int,
+          username: String,
+          client: String = "client",
+          is_anon: Boolean = false,
+          platform: String = "unix",
+          version: Double = 0.11,
+          perms: List[String] = perms)
+
+        case class Response(
+          bufs: Map[String, Buffer],
+          owner: String,
+          tree: List[Tree],
+          users: Map[String, User],
+          room_name: String = "main-room",
+          max_size: Int = 52428800,
+          secret: Boolean = false,
+          server_id: String = "main-server",
+          temp_data: List[String] = List.empty,
+          terms: List[String] = List.empty,
+          perms: List[String] = perms,
+          anon_perms: List[String] = anon_perms)
+
+        object MyJsonProtocol extends DefaultJsonProtocol {
+          implicit val bufferFormat = jsonFormat4(Buffer)
+          implicit val treeFormat = jsonFormat2(Tree)
+          implicit val userFormat = jsonFormat7(User)
+          implicit val connectionResponseFormat = jsonFormat12(Response)
+        }
       }
-      import MyJsonProtocol._
+      import Connection._
+      import Connection.MyJsonProtocol._
 
-      val test = ConnectionResponse(
-        List(Buffer(1, "/path", "abc")),
-        max_size = 52428800,
+      val test = Connection.Response(
+        bufs = Map("1" -> Buffer(1, "README.md", "abc")),
         owner = "user",
-        room_name = "test1",
-        secret = false,
-        server_id = "server1",
-        tree = List.empty
+        tree = List(Tree(filename = "README.md", id = 1)),
+        users = Map("1" -> User(user_id = 1, username = "user"))
       )
 
       println("---------------------------")
