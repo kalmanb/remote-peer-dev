4e3a16641497d7726286ced90babf03e5156622c
kalmanb <kalman@movio.co>
2014-04-22 23:13:15 +1200

    Add working patches Move Rest into main

--- src/test/scala/com/kalmanb/peerdev/PatchTest.scala
+++ src/test/scala/com/kalmanb/peerdev/PatchTest.scala
@@ -5,13 +5,19 @@ import com.kalmanb.test.TestSpec
 class PatchTest extends TestSpec {
 
   describe("DiffMatchPatch") {
-    it("test it") {
-      val start = "123\n"
-      val end = "123\n4"
+    it("it should apply patches correctly") {
+      val orig = "123\n"
+      val patch = "@@ -1,4 +1,5 @@\n 123%0A\n+4\n"
+      val expected = "123\n4"
 
       import com.sksamuel.diffpatch.DiffMatchPatch
+      import com.sksamuel.diffpatch.DiffMatchPatch.Patch
       val dmp = new DiffMatchPatch
-      println(dmp.patch_make(start, end))
+
+      val patches = dmp.patch_fromText(patch).asInstanceOf[java.util.LinkedList[Patch]]
+      val result = dmp.patch_apply(patches, orig)(0)
+
+      result should be(expected)
     }
   }
 }
