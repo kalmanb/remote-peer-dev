7ea2e19db487a01a7b352a0d869ab9b421368e7b
kalmanb <kalman@movio.co>
2014-05-08 22:40:39 +1200

    Clean up tests

--- src/main/scala/com/kalmanb/peerdev/PatchUtils.scala
+++ src/main/scala/com/kalmanb/peerdev/PatchUtils.scala
@@ -5,8 +5,8 @@ import com.sksamuel.diffpatch.DiffMatchPatch.Patch
 
 object PatchUtils {
   val dmp = new DiffMatchPatch
-  def applyPatches(patches: String, text: String): String = {
 
+  def applyPatches(patches: String, text: String): String = {
     val patchList = dmp.patch_fromText(patches).asInstanceOf[java.util.LinkedList[Patch]]
     (dmp.patch_apply(patchList, text)(0)).asInstanceOf[String]
   }
